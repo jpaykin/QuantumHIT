4a4c364b12da717532d98b894c594b6dedb5f56f
jpaykin <jpaykin@gmail.com>
2017-09-25 16:00:35 -0400

    Using groupoid quotients to define QType

--- HIT.v
+++ HIT.v
@@ -20,19 +20,23 @@ Section QType.
 (* Define a HIT on quantum types *)
 Private Inductive QType := 
 | Qubit 
-(*| Tensor (Q1 : QType) (Q2 : QType)*)
+| Tensor (Q1 : QType) (Q2 : QType)
 | Lolli (Q1 Q2 : QType)
+| Lower : forall (τ : Type) `{DecidablePaths τ}, QType
 (*| H : Qubit = Qubit
 | H_dag : H^ = H.*)
 .
-(*Notation "Q1 ⊗ Q2" := (Tensor Q1 Q2) (at level 20).*)
+Notation "Q1 ⊗ Q2" := (Tensor Q1 Q2) (at level 20).
 Notation "Q1 ⊸ Q2" := (Lolli Q1 Q2) (at level 20).
+
 Axiom H : Qubit = Qubit.
 Axiom H_dag : H^ = H. 
 
 Variable P : QType -> Type.
 Variable P_Qubit : P Qubit. 
+Variable P_Tensor : forall Q1, P Q1 -> forall Q2, P Q2 -> P (Q1 ⊗ Q2).
 Variable P_Lolli : forall Q1, P Q1 -> forall Q2, P Q2 -> P (Q1 ⊸ Q2).
+Variable P_Lower : forall (τ : Type) `{DecidablePaths τ}, P (Lower τ).
 Variable P_H : transport P H P_Qubit = P_Qubit. 
 
 Definition P_H' : transport P H^ P_Qubit = P_Qubit :=
@@ -40,11 +44,12 @@ Definition P_H' : transport P H^ P_Qubit = P_Qubit :=
 Variable P_H_dag : P_H' = transport2 _ H_dag P_Qubit @ P_H.
 
 
-
 Fixpoint QType_rect (Q : QType) : P Q := 
   (match Q with
   | Qubit => fun _ _ => P_Qubit
+  | Tensor Q1 Q2 => fun _ _ => P_Tensor _ (QType_rect Q1) _ (QType_rect Q2)
   | Lolli Q1 Q2 => fun _ _ => P_Lolli _ (QType_rect Q1) _ (QType_rect Q2)
+  | Lower τ _ => fun _ _ => P_Lower τ _
   end) P_H P_H_dag.
 Axiom QType_rect_H : apD QType_rect H = P_H.
 
@@ -60,7 +65,7 @@ Axiom QType_rect_H_dag : apD02 QType_rect H_dag = P_H_dag'.
 
 End QType.
 Check QType_rect_H.
-(*Notation "Q1 ⊗ Q2" := (Tensor Q1 Q2) (at level 20).*)
+Notation "Q1 ⊗ Q2" := (Tensor Q1 Q2) (at level 20).
 Notation "Q1 ⊸ Q2" := (Lolli Q1 Q2) (at level 20).
 
 
@@ -79,14 +84,19 @@ Defined.
 
 Variable C : Type.
 Variable C_Qubit : C.
+Variable C_Tensor : C -> C -> C.
 Variable C_Lolli : C -> C -> C.
+Variable C_Lower : Type -> C.
+
 Variable C_H : C_Qubit = C_Qubit.
 Variable C_H_dag : C_H^ = C_H.
 
 
 Definition P := fun (_ : QType) => C.
 Definition P_Qubit := C_Qubit.
+Definition P_Tensor := fun (_ : QType) x (_ : QType) y => C_Tensor x y.
 Definition P_Lolli := fun (_ : QType) x (_ : QType) y => C_Lolli x y.
+
 Definition P_H : transport (fun _ => C) H C_Qubit = C_Qubit :=
     transport_const _ _ @ C_H.
 Definition P_H_dag : P_H' (fun _ => C) C_Qubit P_H 
@@ -110,7 +120,7 @@ Defined.
 Definition QType_rec : QType -> C.
 (*  QType_rect _ C_Qubit (fun _ C1 _ C2 => C_Lolli C1 C2) C_H _ Q.*)
 Proof. 
-  exact (QType_rect P C_Qubit P_Lolli P_H P_H_dag).
+  exact (QType_rect P C_Qubit P_Tensor P_Lolli (fun τ _ => C_Lower τ) P_H P_H_dag).
 Defined.
 
 (* TODO: computation rules for QType_rec *)
@@ -133,7 +143,7 @@ Lemma QType_rec_H : ap QType_rec H = C_H.
 Proof. 
   unfold QType_rec.
   refine (apD_const' _ _ @ _); simpl.
-  refine (1 @@ QType_rect_H _ _ _ _ _ @ _). 
+  refine (1 @@ QType_rect_H _ _ _ _ _ _ _ @ _). 
   refine (concat_p_pp _ _ _ @ _).
   refine (concat_Vp _ @@ 1 @ _).
   refine (concat_1p _).
@@ -154,7 +164,7 @@ Definition C_H_dag' : apD QType_rec H^
 Proof.
   refine (apD_V _ _ @ _). 
   refine (transport (R P P_Qubit) _ P_H_dag).
-  refine (QType_rect_H P P_Qubit P_Lolli P_H P_H_dag)^.
+  refine (QType_rect_H P P_Qubit P_Tensor P_Lolli (fun τ _ => C_Lower τ) P_H P_H_dag)^.
 (*refine (apD_const _ _ @ _). 
   refine (1 @@ (QType_rec_H' @ C_H_dag) @ _).
   refine (_ @ (1 @@ (apD_const _ _)^)).
@@ -167,22 +177,27 @@ Defined.
 
 Lemma QType_rec_H_dag : apD02 QType_rec H_dag = C_H_dag'.
 Proof.
-  exact (QType_rect_H_dag P P_Qubit P_Lolli P_H P_H_dag).
+  exact (QType_rect_H_dag P P_Qubit P_Tensor P_Lolli (fun τ _ => C_Lower τ) P_H P_H_dag).
 Defined.
 
 End QType_rec.
-Arguments QType_rec C C_Qubit C_Lolli C_H C_H_dag Q : assert.
+Arguments QType_rec C C_Qubit C_Tensor C_Lolli C_Lower C_H C_H_dag Q : assert.
 
 (* We can define a recursion principle that collapses all unitaries,
    sending them all to 1. *)
-Definition QType_rec_triv (C : Type) (C_Q : C) (C_Lolli : C -> C -> C) 
+Definition QType_rec_triv (C : Type) (C_Q : C) 
+          (C_Tensor : C -> C -> C) (C_Lolli : C -> C -> C) (C_Lower : Type -> C)
                           : QType -> C :=
-  QType_rec C C_Q C_Lolli 1 1.
+  QType_rec C C_Q C_Tensor C_Lolli C_Lower 1 1.
 
 
 Definition toClassical (Q : QType) : Type :=
-  QType_rec_triv _ Bool (fun τ1 τ2 => τ1 -> τ2) Q.
-Definition QType_size : QType -> nat := QType_rec_triv nat 1%nat (fun n1 n2 => n1+n2)%nat.
+  QType_rec_triv _ Bool (fun τ1 τ2 => τ1 * τ2) 
+                        (fun τ1 τ2 => τ1 -> τ2) 
+                        (fun τ => τ) Q.
+Definition QType_size : QType -> nat := 
+    QType_rec_triv nat 1%nat (fun n1 n2 => n1+n2)%nat (fun n1 n2 => n1+n2)%nat 
+      (fun _ => 1%nat).
 
 Require Import Peano.
 Open Scope nat.
@@ -224,15 +239,24 @@ Lemma QType_size_pos : forall Q, 0 < QType_size Q.
 Proof. 
   set (P Q := 0 < QType_size Q).
   assert (P_Qubit : 0 < 1) by constructor. 
+  assert (P_Tensor : forall Q1 (H1 : P Q1) Q2 (H2 : P Q2), P (Tensor Q1 Q2)).
+  { 
+    unfold P; intros; simpl.
+    apply lt_plus_r. auto.
+  } 
   assert (P_Lolli : forall Q1 (H1 : P Q1) Q2 (H2 : P Q2), P (Lolli Q1 Q2)).
   { 
     unfold P; intros; simpl.
     apply lt_plus_r. auto.
   } 
+  assert (P_Lower : forall τ `{DecidablePaths τ}, P (Lower τ)).
+  { 
+    unfold P; intros; simpl. auto.
+  }
   assert (P_H : transport P H P_Qubit = P_Qubit) by apply path_ishprop.
   assert (P_H_dag : P_H' P P_Qubit P_H = transport2 P H_dag P_Qubit @ P_H) 
     by apply path_ishprop.
-  apply (QType_rect P P_Qubit P_Lolli P_H P_H_dag).
+  apply (QType_rect P P_Qubit P_Tensor P_Lolli P_Lower P_H P_H_dag).
 Qed.
 
 (* Decidability *)
@@ -281,280 +305,17 @@ Proof.
   set (P := fun (Q : QType) => forall R, Decidable (Q = R)).
   change (forall (Q : QType), P Q).
   assert (P_Qubit : P Qubit). admit.
+  assert (P_Tensor : forall Q1, P Q1 -> forall Q2, P Q2 -> P (Q1 ⊗ Q2)). admit.
   assert (P_Lolli : forall Q1, P Q1 -> forall Q2, P Q2 -> P (Q1 ⊸ Q2)). admit.
+  assert (P_Lower : forall (τ : Type) `{DecidablePaths τ}, P (Lower τ)).
+  { intros τ decτ.
+    unfold P.
+    admit.
+  }
   assert (P_H : transport P H P_Qubit = P_Qubit). admit.
   assert (P_H_dag : P_H' P P_Qubit P_H = transport2 P H_dag P_Qubit @ P_H). admit.
-  apply (QType_rect P P_Qubit P_Lolli P_H P_H_dag). 
+  apply (QType_rect P P_Qubit P_Tensor P_Lolli P_Lower P_H P_H_dag). 
 Admitted.
 
 
-(*
-(************)
-(** Syntax **)
-(************)
 
-Section Syntax.
-
-
-Set Implicit Arguments.
-
-Inductive Exp X :=
-| Var : X -> Exp X
-| App : Exp X -> Exp X -> Exp X
-| Abs : Exp (option X) -> Exp X.
-
-
-Inductive Exp' X : QType -> Type :=
-| Var' : forall Q, X -> Exp' X Q
-| App' : forall Q R, Exp' X (Q ⊸ R) -> Exp' X Q -> Exp' X R
-| Abs' : forall Q R, Exp' (option X) R -> Exp' X (Q ⊸ R).
-
-(*
-Inductive LExp : forall {X} `{DecidablePaths X}, Ctx X QType -> QType -> Type :=
-| LVar : forall {X} `{DecidablePaths X} {Γ : Ctx X QType} (x : X) {Q : QType},
-         Γ = singleton x Q -> 
-         LExp Γ Q 
-| LApp : forall {X} `{DecidablePaths X} {Γ1 Γ2 Γ : Ctx X QType} {Q1 Q2 : QType},
-         Γ = Γ1 · Γ2 ->
-         LExp Γ1 (Q1 ⊸ Q2) ->
-         LExp Γ2 Q1 ->
-         LExp Γ Q2
-| LAbs : forall {X} `{DecidablePaths X} {Γ : Ctx X QType} {Q1 Q2},
-         LExp (extend Γ Q1) Q2 ->
-         LExp Γ (Q1 ⊸ Q2)
-.
-*)
-
-Definition exp_fmap {X Y : Type} (f : X -> Y) (e : Exp X) : Exp Y.
-Proof.
-  generalize dependent Y.
-  induction e; intros Y f.
-  - exact (Var (f x)).
-  - exact (App (IHe1 Y f) (IHe2 Y f)).
-  - exact (Abs (IHe (option Y) (fmap f))).
-Defined.
-Instance expF : Functor Exp := {fmap := @exp_fmap}.
-
-
-
-Definition shift {A} (e : Exp A) : Exp (option A) := fmap (@Some A) e.
-Definition exp_option_liftT {A B : Set} (f : A -> Exp B) (x : option A) 
-                            : Exp (option B) :=
-  match x with
-  | None => Var None
-  | Some y => shift (f y)
-  end.
-
-Fixpoint exp_bind {A : Set} (e : Exp A) : forall {B : Set}, (A -> Exp B) -> Exp B :=
-  match e with
-  | Var x => fun B f => f x
-  | App e1 e2 => fun B f => App (exp_bind e1 f) (exp_bind e2 f)
-  | Abs e' => fun B f => Abs (exp_bind e' (exp_option_liftT f))
-  end.
-
-(* may not be the most efficient *)
-Fixpoint exp_liftA {A B : Set} (f : Exp (A -> B)) (e : Exp A) : Exp B :=
-  exp_bind f (fun f' => fmap f' e). 
-
-Instance expA : Applicative Exp := {pure := Var;
-                                    liftA := @exp_liftA}.
-Instance expM : Monad Exp := {bind := @exp_bind}.
-
-Definition subst_var {Γ} (e : Exp Γ) (x : option Γ) : Exp Γ :=
-  match x with
-  | None => e
-  | Some y => Var y
-  end.
-
-(* Substitute e for the variable 0=None in e' *)
-Definition subst {Γ} (e : Exp Γ) (e' : Exp (option Γ)) : Exp Γ :=
-  do x ← e'; subst_var e x.
-
-(* Shouldn't need congruence rules if we extend this to an equivalence relation *)
-Inductive eq1 {X} : Exp X -> Exp X -> Type :=
-| App_η : forall (e : Exp X), eq1 e (Abs (App (shift e) (Var None)))
-| App_β : forall e1 e2, eq1 (App (Abs e1) e2) (subst e2 e1)
-.
-
-Inductive Val : Type :=
-| VAbs : Exp (option Empty) -> Val.
-
-Definition QCtx X := Ctx X QType.
-
-Inductive WF_Exp : forall {X : Type} `{DecidablePaths X}, QCtx X -> Exp X -> QType -> Type :=
-| WF_Var : forall {X : Type} `{DecidablePaths X} (Γ : QCtx X) (x : X) (Q : QType),
-           Γ = singleton x Q -> WF_Exp Γ (Var x) Q
-| WF_App : forall {X : Type} `{DecidablePaths X} (Γ1 Γ2 Γ : QCtx X) (Q R : QType) (e1 e2 : Exp X),
-           Γ = Γ1 · Γ2 -> 
-           WF_Exp Γ1 e1 (Q ⊸ R) ->
-           WF_Exp Γ2 e2 Q ->
-           WF_Exp Γ  (App e1 e2) R
-| WF_Abs : forall {X : Type} `{DecidablePaths X} (Γ : QCtx X) Q R (e' : Exp (option X)),
-           WF_Exp (extend Γ Q) e' R ->
-           WF_Exp Γ (Abs e') (Q ⊸ R)
-.
-
-Inductive LExp : forall {X : Type}, QCtx X -> QType -> Type :=
-| LVar : forall {X : Type} (Γ : QCtx X) (x : X) (Q : QType),
-         Γ = singleton x Q -> LExp Γ Q
-| LApp : forall {X : Type} `{DecidablePaths X} (Γ1 Γ2 Γ : QCtx X) (Q R : QType),
-         Γ = Γ1 · Γ2 -> Γ <> None ->
-         LExp Γ1 (Q ⊸ R) ->
-         LExp Γ2 Q ->
-         LExp Γ R
-| LAbs : forall {X : Type} (Γ : QCtx X) Q R,
-         LExp (extend Γ Q) R ->
-         LExp Γ (Q ⊸ R)
-.
-Arguments LVar {X} {Γ} x {Q} : rename.
-Arguments LApp {X} {decX} {Γ1 Γ2 Γ} {Q R} : rename.
-Arguments LAbs {X} {Γ} {Q R} e : rename.
-
-
-Definition lexp_fmap' {X Y : Type} `{DecidablePaths X} `{DecidablePaths Y}
-                              (f : X -> Y) (Γ : Ctx X QType)
-                              (Q: QType)
-                              (e : LExp Γ Q) : LExp (fmap f Γ) Q.
-  generalize dependent Y.
-  induction e as [X Γ x Q pf1 | |]; intros Y HY f.
-  - refine (LVar (f x) _). rewrite pf1. apply fmap_singleton; auto.
-  - refine (LApp _ _ (IHe1 _ _ _ f) (IHe2 _ _ _ f)).
-    * rewrite p. apply fmap_merge.
-    * intros H.
-      apply n.
-      apply (fmap_None_inv _ _ _ _ H).
-  - set (e' := IHe _ (option Y) _ (fmap f)).
-    rewrite fmap_extend in e'; auto.
-    refine (LAbs e').
-Defined.
-
-Definition EExp X Q := exists (Γ : QCtx X), LExp Γ Q.
-
-Definition eVar {X} (x : X) (Q : QType) : EExp X Q.
-  exists (singleton x Q). 
-  exact (LVar x idpath).
-Defined. 
-
-About QType_rec.
-
-Instance decQCtx {X} `{DecidablePaths X} : DecidablePaths (QCtx X).
-Admitted.
-
-Definition eApp {X} `{DecidablePaths X} Q R (e1 : EExp X (Q ⊸ R)) (e2 : EExp X Q) 
-           : option (EExp X R).
-Proof.
-  destruct e1 as [Γ1 e1].
-  destruct e2 as [Γ2 e2].
-  destruct (dec_paths (Γ1 · Γ2) None) as [H | H].
-  - exact None.
-  - apply Some. exists (Γ1 · Γ2). exact (LApp idpath H e1 e2).
-Defined.
-
-(* goes in SetCtx.v *)
-Definition lookup {X} (Γ : QCtx X) (x : X) : option QType.
-Admitted.
-Definition tail {X} (Γ : QCtx (option X)) : QCtx X.
-Admitted.
-Lemma extend_tail : forall {X} (Γ : QCtx (option X)) Q,
-      lookup Γ None = Some Q -> extend (tail Γ) Q = Γ.
-Admitted.
-
-
-Definition eAbs {X} Q R (e' : EExp (option X) R) : option (EExp X (Q ⊸ R)).
-Proof.
-  destruct e' as [Γ e'].
-  destruct (dec_paths (lookup Γ None) (Some Q)).
-  - (* lookup Γ None = Some Q *)
-    apply Some. exists (tail Γ). apply LAbs. rewrite (extend_tail p). exact e'.
-  - exact None.
-Defined.
-
-
-
-Definition EExp_fmap {Q} {X Y : Type} `{DecidablePaths X} `{DecidablePaths Y}
-                     (f : X -> Y) (e : EExp X Q) : EExp Y Q.
-Proof.
-  destruct e as [Γ e].
-  exists (fmap f Γ). exact (lexp_fmap' f e).
-Defined.
-(* Can't give this a functor instance per se because it depends on the fact that X and Y are decidable. *)
-
-Definition eshift {Q} {X} `{DecidablePaths X} (e : EExp X Q) : EExp (option X) Q := 
-    EExp_fmap (@Some X) e.
-
-Definition EExp_liftT {Q} {X Y : Type} `{DecidablePaths X} `{DecidablePaths Y} (f : X -> EExp Y Q)
-                      (x : option X) : EExp (option Y) Q :=
-  match x with
-  | None => eVar None Q
-  | Some x => eshift (f x)
-  end.
-
-Definition EExp_bind' {X : Type} {Q} (e : EExp X Q) : forall {Y : Type} {R}, 
-                      (X -> EExp Y R) -> EExp Y R.
-Proof.
-  destruct e as [Γ e].
-  induction e; intros Y R f.
-  - exact (f x).
-  - admit.
-  - admit.
-Admitted.
-
-Fixpoint exp_bind {A : Set} (e : Exp A) : forall {B : Set}, (A -> Exp B) -> Exp B :=
-  match e with
-  | Var x => fun B f => f x
-  | App e1 e2 => fun B f => App (exp_bind e1 f) (exp_bind e2 f)
-  | Abs e' => fun B f => Abs (exp_bind e' (exp_option_liftT f))
-  end.
-
-(* may not be the most efficient *)
-Fixpoint exp_liftA {A B : Set} (f : Exp (A -> B)) (e : Exp A) : Exp B :=
-  exp_bind f (fun f' => fmap f' e). 
-
-Instance expA : Applicative Exp := {pure := Var;
-                                    liftA := @exp_liftA}.
-Instance expM : Monad Exp := {bind := @exp_bind}.
-
-Definition subst_var {Γ} (e : Exp Γ) (x : option Γ) : Exp Γ :=
-  match x with
-  | None => e
-  | Some y => Var y
-  end.
-
-(* Substitute e for the variable 0=None in e' *)
-Definition subst {Γ} (e : Exp Γ) (e' : Exp (option Γ)) : Exp Γ :=
-  do x ← e'; subst_var e x.
-
-
-
-Definition unitary {X} {Q1 Q2} (U : Q1 = Q2) {Γ : Ctx X} (e : QExp Γ Q1) : QExp Γ Q2 :=
-  transport _ U e.
-
-Definition Unitary (Q : QType) := Q = Q.
-
-Lemma U_compose : forall (Q1 Q2 Q3 : QType) (U1 : Q1 = Q2) (U2 : Q2 = Q3)
-                  {X} (Γ : Ctx X) (e : QExp Γ Q1),
-      unitary U2 (unitary U1 e) = unitary (U1 @ U2) e.
-Proof.
-  destruct U1. intros. simpl. Search (1 @ _ = _). rewrite concat_1p. reflexivity.
-Qed.
-
-Lemma U_U_transpose : forall (Q : QType) (U : Unitary Q) {X} (Γ : Ctx X) (e : QExp Γ Q),
-      unitary (U^) (unitary U e) = e.
-Proof. 
-  intros. rewrite U_compose. Search (?p @ ?p^). rewrite concat_pV. reflexivity.
-Qed.
-
-Lemma H_H_inverse : forall {X} (Γ : Ctx X) (e : QExp Γ Qubit), unitary H (unitary H e) = e.
-Proof.
-  intros. 
-  apply (@concat _ _ (unitary H^ (unitary H e))).
-  - rewrite H_dag; auto.
-  - apply U_U_transpose.
-Qed.
-
-Definition QType_size (Q : QType) : nat.
-Proof.
-  apply 
-
-End Syntax.
-*)
--- Syntax.v
+++ Syntax.v
@@ -16,29 +16,47 @@ Inductive Extend {A} (a : A) (X : Type) : Type :=
 Arguments hd {A} {a} {X}.
 Arguments tl {A} {a} {X}.
 
-Definition extend {X A : Type} (Γ : Ctx X A) (a : A) : Ctx (Extend a X) A.
-(*
-  do Γ' ← (Γ : option (PreCtx A X));
-  return_ (add (hd,a) (shift Γ')).
-*)
-Admitted.
+Definition extend {X A : Type} (Γ : Ctx X A) (a : A) : Ctx (Extend a X) A :=
+  fmap_Ctx tl Γ.
 
 
-Definition extend_fmap (Q : QType) {X Y : Type} (f : X -> Y) (x : Extend Q X) : Extend Q Y :=
+Definition extend_fmap {A : Type} (a : A) {X Y : Type} (f : X -> Y) 
+                       (x : Extend a X) : Extend a Y :=
   match x with
   | hd => hd
   | tl x => tl (f x)
   end.
 
-Instance extend_functor Q : Monad.Functor (Extend Q) := {fmap := @extend_fmap Q}.
+Instance extend_functor {A} (a : A) : Monad.Functor (Extend a) := 
+         {fmap := @extend_fmap _ a}.
 
 Inductive Exp X : QType -> Type :=
 | Var : forall {Q}, X -> Exp X Q
 | App : forall {Q R}, Exp X (Q ⊸ R) -> Exp X Q -> Exp X R
-| Abs : forall {Q R}, Exp (Extend Q X) R -> Exp X (Q ⊸ R).
+| Abs : forall {Q R}, Exp (Extend Q X) R -> Exp X (Q ⊸ R) 
+| Pair : forall {Q R}, Exp X Q -> Exp X R -> Exp X (Q ⊗ R)
+| LetPair : forall {Q1 Q2 R}, 
+            Exp X (Q1 ⊗ Q2) -> Exp (Extend Q1 (Extend Q2 X)) R -> Exp X R
+| Put : forall {τ} `{DecidablePaths τ}, τ -> Exp X (Lower τ)
+| LetBang : forall {τ Q} `{DecidablePaths τ}, 
+  Exp X (Lower τ) -> (τ -> Exp X Q) -> Exp X Q
+| new  : Bool -> Exp X Qubit
+| meas : Exp X Qubit -> Exp X (Lower Bool)
+.
+
 Arguments Var {X} {Q} x.
 Arguments App {X} {Q R}.
 Arguments Abs {X} {Q R}.
+Arguments Pair {X} {Q R}.
+Arguments LetPair {X} {Q1 Q2 R}.
+Arguments Put {X} {τ} {decτ} (x) : rename.
+Arguments LetBang {X τ Q} {decτ} : rename.
+Arguments new {X}.
+Arguments meas {X}.
+
+(* These axioms should be incorporated into the equational theory *)
+Axiom new_meas : forall X b, meas (@new X b) = Put (negb b).
+
 
 Definition exp_fmap {X Y : Type} (f : X -> Y) {Q} (e : Exp X Q) : Exp Y Q.
 Proof.
@@ -47,9 +65,16 @@ Proof.
   - exact (Var (f x)).
   - exact (App (IHe1 Y f) (IHe2 Y f)).
   - exact (Abs (IHe (Extend Q Y) (fmap f))).
+  - exact (Pair (IHe1 _ f) (IHe2 _ f)).
+  - exact (LetPair (IHe1 _ f) (IHe2 _ (fmap (fmap f)))).
+  - exact (Put τ0).
+  - exact (LetBang (IHe _ f) (fun x => X0 x _ f)).
+  - exact (new b).
+  - exact (meas (IHe _ f)).
 Defined.
 
-Definition shift {X} {Q R : QType} (e : Exp X R) : Exp (Extend Q X) R := exp_fmap tl e.
+Definition shift {X} {Q R : QType} (e : Exp X R) : Exp (Extend Q X) R := 
+    exp_fmap tl e.
 
 Definition exp_option_lift {X Y : Type} {Q R} (f : X -> Exp Y R) (x : Extend Q X) 
                            : Exp (Extend Q Y) R :=
@@ -69,11 +94,27 @@ Proof.
            do e2 ← IHe2 _ f;
            return_ (App e1 e2)).
   - apply (fmap Abs).
-    apply IHe.
-    intros S [ | z]. 
-    * exact (Some (Var hd)).
-    * exact (fmap shift (f _ z)).
-Defined.
+    exact (IHe _ (fun S ext => match ext with
+                               | hd => Some (Var hd)
+                               | tl z => fmap shift (f _ z)
+                               end)).
+  - exact (do e1 ← IHe1 _ f;
+           do e2 ← IHe2 _ f;
+           return_ (Pair e1 e2)).
+  - refine (do e1 ← IHe1 _ f;
+            do e2 ← IHe2 _ _;
+            return_ (LetPair e1 e2)).
+    refine (fun S ext => match ext with
+                         | hd => (* 0:Q1 ↦ Var 0 *) Some (Var hd)
+                         | tl hd => Some (Var (tl hd)) (* 1:Q2 ↦ Var 1 *)
+                         | tl (tl z) => fmap shift (fmap shift (f _ z))
+                         end).
+  - refine (Some (Put τ0)).
+  - refine (do e1 ← IHe _ f; _).
+    admit (*??? *).
+  - refine (Some (new b)).
+  - exact (do e' ← IHe _ f; return_ (meas e')).
+Admitted.
 
 Definition subst_var {X} {R} (e : Exp X R) (x : Extend R X) : Exp X R :=
   match x with
@@ -108,11 +149,11 @@ Notation "(| e |)" := (class_of (@trunc_exp_step _ _) e) (at level 40).
 
 (* unitaries *)
 
-
 Definition unitary {X} {Q1 Q2} (U : Q1 = Q2) (e : Exp X Q1) : Exp X Q2 :=
   transport _ U e.
 
 Definition Unitary (Q : QType) := Q = Q.
+
 
 Lemma U_compose : forall (Q1 Q2 Q3 : QType) (U1 : Q1 = Q2) (U2 : Q2 = Q3)
                   {X} (e : Exp X Q1),
@@ -167,5 +208,32 @@ Corollary U_lolli_unitary : forall X Q1 Q2
           (| unitary (U1 -u⊸ U2) e |)
         = (| abs(fun x => unitary U2 (shift e `app` unitary U1^ (Var x))) |).
 Proof.
+  intros.
+  apply U_lolli_unitary'.
+Defined.
+
+Lemma unitary_id : forall X Q (e : Exp X Q), e = unitary idpath e.
+Proof. reflexivity. Defined.
+
+(* CANNOT prove this *)
+Lemma U_not_id : forall Q (U : Q = Q) X (e : Exp X Q),
+                 unitary U e = e ->
+                 U = 1%path.
+Abort.
 
 
+Instance toClassical_Deciable Q : DecidablePaths (toClassical Q).
+Admitted.
+
+Definition meas_any {Q : QType} {X} (e : Exp X Q) : Exp X (Lower (toClassical Q)).
+Proof.
+  generalize dependent X.
+Admitted.
+
+(* This was an axiom, but we can prove it instead! *)
+Lemma unitary_discard : forall Q1 Q2 (U : Q1 = Q2) 
+                        X Q (e : Exp X Q1) (e' : Exp X Q),
+   LetBang (meas_any (unitary U e)) (fun _ => e') = LetBang (meas_any e) (fun _ => e').
+Proof.
+  induction U; reflexivity.
+Defined.
